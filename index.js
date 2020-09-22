e8a6bff720bee09184c8d625639d80c5e425a56f
Jarom Plewe <jplewe23@gmail.com>
2020-09-22 13:53:13 -0600

    added questions

--- index.js
+++ index.js
@@ -3,11 +3,61 @@ const inquirer = require('inquirer');
 
 // array of questions for user
 const questions = [
-
+    {
+        type: "input",
+        name: "title",
+        message: "What is the title for your project?"
+    },
+    {
+        type: "input",
+        name: "description",
+        message: "Enter a description for your project."
+    },
+    {
+        type: "input",
+        name: "installationInstructions",
+        message: "Enter installation instructions."
+    },
+    {
+        type: "input",
+        name: "usageInformation",
+        message: "Enter usage information."
+    },
+    {
+        type: "input",
+        name: "contributionGuidlines",
+        message: "Enter contribution guidlines."
+    },
+    {
+        type: "input",
+        name: "testInstructions",
+        message: "Enter test instructions."
+    },
+    {
+        type: "list",
+        name: "license",
+        message: "Please choose a license.",
+        choices: [
+            "choice 1",
+            "choice 2",
+            "choice 3"
+        ]
+    },
+    {
+        type: "input",
+        name: "github",
+        message: "Enter your github username."
+    },
+    {
+        type: "input",
+        name: "email",
+        message: "Enter your email address."
+    },
 ];
 
 // function to write README file
 function writeToFile(fileName, data) {
+
 }
 
 // function to initialize program
